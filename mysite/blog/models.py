9749c2148ca1745d951140cbdf16edab6aa8ddb0
Corledon <bartosz.bienczyk@gmail.com>
2025-11-26 23:30:57 +0100

    Kolejny update

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
-from django.db import models
 from django.utils import timezone
+from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 
@@ -17,17 +18,18 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Opublikowany'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='blog_posts')
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=2,
-        choices=Status.choices,
-        default=Status.DRAFT)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
     object = models.Manager()
     published = PublishManager()
 
@@ -39,3 +41,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug]
+                       )
